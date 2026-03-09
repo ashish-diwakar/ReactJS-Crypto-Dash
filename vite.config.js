56bc2032aedc6e54f2a2298d09e525f7cef1f6e9
ashish-diwakar <77900908+ashish-diwakar@users.noreply.github.com>
2026-03-09 23:45:55 +0530

    Just updated vite.config.js file
    Removed test build directory so that code can build to default dist folder.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-    assetsDir: 'myAssets',
-    minify: true
-  },
   server: {
     proxy: {
       // Proxy requests starting with '/api'
